5fd25fd35979620a0fe9b29223282aa1ac5b8235
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2023-03-19 14:25:03 +0300

    weview

---
+++ lib/screens/BrowserScreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class BrowserScreen extends StatefulWidget {
+  const BrowserScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BrowserScreen> createState() => _BrowserScreenState();
+}
+
+class _BrowserScreenState extends State<BrowserScreen> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Stack(
+        children: [
+          InAppWebView(
+            initialUrlRequest:  URLRequest(url: Uri.parse("http://kdrc.ru/")),
+            onWebViewCreated: (controller){
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
