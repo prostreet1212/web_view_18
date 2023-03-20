195bcfa410dbb9d4ced93cc090e7c5c3a240bf4c
prostreet1212 <prostreet1212@gmail.com>
2023-03-20 16:54:32 +0300

    webview

--- lib/screens/BrowserScreen.dart
+++ lib/screens/BrowserScreen.dart
@@ -10,18 +10,64 @@ class BrowserScreen extends StatefulWidget {
 
 class _BrowserScreenState extends State<BrowserScreen> {
 
+  TextEditingController urlTextController=TextEditingController();
+  InAppWebViewController? webViewController;
 
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Stack(
+      appBar: AppBar(
+        title: Text('InAppWebView'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          InAppWebView(
-            initialUrlRequest:  URLRequest(url: Uri.parse("http://kdrc.ru/")),
-            onWebViewCreated: (controller){
-            },
+          Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                onPressed: null,
+              ),
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: null,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(12),
+                  child: TextField(
+                    controller: urlTextController,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[300],
+                      hintText: 'Введите url',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(24),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.all(12)
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          //LinearProgressIndicator(),
+          Expanded(
+            child: InAppWebView(
+              initialUrlRequest: URLRequest(url: Uri.parse("http://kdrc.ru/")),
+              onWebViewCreated: (controller) {
+                webViewController=controller;
+              },
+            ),
           )
         ],
       ),
