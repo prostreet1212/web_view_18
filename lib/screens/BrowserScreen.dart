aa6c0d5b679cd32155bb193e9d57739dc2b8fced
prostreet1212 <prostreet1212@gmail.com>
2023-03-25 18:33:24 +0300

    webview

--- lib/screens/BrowserScreen.dart
+++ lib/screens/BrowserScreen.dart
@@ -9,68 +9,102 @@ class BrowserScreen extends StatefulWidget {
 }
 
 class _BrowserScreenState extends State<BrowserScreen> {
-
-  TextEditingController urlTextController=TextEditingController();
+  TextEditingController urlTextController = TextEditingController();
   InAppWebViewController? webViewController;
-
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+          useShouldOverrideUrlLoading: true,
+          mediaPlaybackRequiresUserGesture: false,
+          allowFileAccessFromFileURLs: true,
+          javaScriptEnabled: true),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('InAppWebView'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.arrow_back),
-                onPressed: null,
-              ),
-              IconButton(
-                icon: Icon(Icons.arrow_forward),
-                onPressed: null,
-              ),
-              IconButton(
-                icon: Icon(Icons.refresh),
-                onPressed: null,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(12),
-                  child: TextField(
-                    controller: urlTextController,
-                    decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.grey[300],
-                      hintText: 'Введите url',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(24),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
-                      ),
-                      contentPadding: EdgeInsets.all(12)
+  void initState() {
+    urlTextController.text = 'https://flutter.dev';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) async{
+    return WillPopScope(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('InAppWebView'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: await  webViewController!.canGoBack()?() {
+                    //webViewController!.goBack();
+                  }:null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: null,
+                ),
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    webViewController!.loadUrl(
+                        urlRequest:
+                            URLRequest(url: Uri.parse(urlTextController.text)));
+                  },
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(12),
+                    child: TextField(
+                      controller: urlTextController,
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.grey[300],
+                          hintText: 'Введите url',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(24),
+                            borderSide: BorderSide(
+                              width: 0,
+                              style: BorderStyle.none,
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.all(12)),
                     ),
                   ),
-                ),
-              )
-            ],
-          ),
-          //LinearProgressIndicator(),
-          Expanded(
-            child: InAppWebView(
-              initialUrlRequest: URLRequest(url: Uri.parse("http://kdrc.ru/")),
-              onWebViewCreated: (controller) {
-                webViewController=controller;
-              },
+                )
+              ],
             ),
-          )
-        ],
+            //LinearProgressIndicator(),
+            Expanded(
+              child: InAppWebView(
+                initialUrlRequest: URLRequest(
+                  url: Uri.parse("http://kdrc.ru/"),
+                ),
+                initialOptions: options,
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+              ),
+            )
+          ],
+        ),
       ),
+      onWillPop: () async {
+        if(await webViewController!.canGoBack()){
+          webViewController!.goBack();
+          return false;
+        }else{
+          return true;
+        }
+      },
     );
   }
 }
